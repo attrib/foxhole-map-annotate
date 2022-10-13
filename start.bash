b15cf80e7437366a4987c359c495aa65355cf5ae
Karl Fritsche <github@attrib.org>
2022-10-13 13:26:09 +0200

    small fixes

--- start.bash
+++ start.bash
@@ -8,7 +8,7 @@ docker run -d \
   -v `pwd`/uploads:/app/uploads \
   -v `pwd`/sessions:/app/sessions \
   -v `pwd`/markers.json:/app/markers.json \
-  -p 3033:3000 \
+  -p 127.0.0.1:3033:3000 \
   --restart always \
   --env-file .env \
   --name born \
