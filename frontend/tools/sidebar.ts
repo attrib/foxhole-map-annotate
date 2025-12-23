2cec823a5472c3792d8bfb5a6dcc33a46ed65fe0
Edelor <ibailbum@gmail.com>
2025-12-23 23:41:24 +0100

    Updated the behaviour on save to only affect polygons and rectangles, requiring notes to be filled in before saving.

--- frontend/tools/sidebar.ts
+++ frontend/tools/sidebar.ts
@@ -102,9 +102,12 @@ class Sidebar {
     }
 
     document.getElementById('save-button').addEventListener('click', () => {
-      if (this.notesInput.value !== undefined && this.notesInput.value !== null && this.notesInput.value.trim() !== '') {
         if (this.editFeature) {
           const type = this.editFeature.get('type')
+          if ((type === 'rectangle' || type === 'polygon') && (this.notesInput.value === undefined || this.notesInput.value === null || this.notesInput.value.trim() === '')) {
+            blinkInput(this.notesInput);
+            return
+          }
           if (type === 'line') {
             this.editFeature.set('clan', this.clanInput.value, true)
           }
@@ -120,10 +123,7 @@ class Sidebar {
           this.editFeature.set('notes', this.notesInput.value)
           tools.emit(tools.EVENT_ICON_UPDATED, this.editFeature)
         }
-      }
-      else {
-        blinkInput(this.notesInput);
-      }
+      
     })
     document.addEventListener('keydown', (event) => {
       if (event.target.nodeName.toLowerCase() === 'input' || event.target.nodeName.toLowerCase() === 'textarea') {
