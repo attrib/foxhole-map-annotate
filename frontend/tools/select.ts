0bb9e36ad448e4186ade3afc69f4f72b559ea6af
Edelor <ibailbum@gmail.com>
2026-02-08 17:23:19 +0100

    Feature info shows selected group if it exists

--- frontend/tools/select.ts
+++ frontend/tools/select.ts
@@ -529,7 +529,7 @@ class Select {
   }
 
   getUser = (feature) => {
-    return feature.get('clan') || feature.get('user') || 'World'
+    return feature.get('displayName') || feature.get('user') || 'World'
   }
 
   updateDecay = (event) => {
